77e7583a83d637a97610b1d1bf921a7002c58e5e
Mostakimw <scientist.expert@gmail.com>
2023-04-06 21:20:15 +0600

    basic layout created and tshirt data loaded

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <nav className="navLink">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+      <Link className="nav-link" to="/about">
+        About
+      </Link>
+      <Link className="nav-link" to="/contact">
+        Contact
+      </Link>
+      <Link className="nav-link" to="/review">
+        Review
+      </Link>
+      {/* <Link to="/"></Link> */}
+    </nav>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Main from "./components/Layout/Main";
+import About from "./components/About/About";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("tshirts.json"),
+      },
+      {
+        path: "about",
+        element: <About></About>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
